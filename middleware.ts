973b8cc79f640223f6f611bae141032d880f92a2
Aliona <aliona.consultations@gmail.com>
2026-06-19 18:20:31 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -124,6 +124,6 @@ export default async function middleware(req: Request): Promise<Response> {
 }
 
 export const config = {
-  matcher: ['/', '/index.html', '/events', '/events/:path*'],
+  matcher: ['/', '/index.html', '/events'],
   runtime: 'edge',
 };
